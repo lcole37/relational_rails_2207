4b560046bb4643e37706017092745a60034e631c
Lucas Colwell <colwellc37@gmail.com>
2022-08-22 22:36:50 -0600

    Add route for passenger index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/flights', to: 'flights#index'
   get '/flights/:id', to: 'flights#show'
+  get '/passengers', to: 'passengers#index'
 end
