db2cb4b782ec046ea15f0834f00f46ef1b9c3402
Lucas Colwell <colwellc37@gmail.com>
2022-08-22 20:55:34 -0600

    Test: update variables, spelling etc

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'flight index page' do
   it 'has the name of each flight record in the system' do
-    maui = Flight.create!(number: "707", airline: "United" destination: "Maui", international?: false)
-    nyc = Flight.create!(number: "423", airline: "Southwest" destination: "New York", international?: false)
-    columbus = Flight.create!(number: "890", airline: "American" destination: "Columbus", international?: false)
-    dc = Flight.create!(number: "398", airline: "Frontier" destination: "DC", international?: false)
-    cancun = Flight.create!(number: "524", airline: "Delta" destination: "Cancun", international?: true)
+    maui = Flight.create!(number: "707", airline: "United", destination: "Maui", international: false)
+    nyc = Flight.create!(number: "423", airline: "Southwest", destination: "New York", international: false)
+    columbus = Flight.create!(number: "890", airline: "American", destination: "Columbus", international: false)
+    dc = Flight.create!(number: "398", airline: "Frontier", destination: "DC", international: false)
+    cancun = Flight.create!(number: "524", airline: "Delta", destination: "Cancun", international: true)
 
 
-    cindi = maui.passengers.create!(name: "Cindi", age: "63", gold_status?: true)
-    harry = maui.passengers.create!(name: "Harry", age: "35", gold_status?: false)
-    larry = maui.passengers.create!(name: "Larry", age: "28", gold_status?: true)
-    mary = maui.passengers.create!(name: "Mary", age: "18", gold_status?: false)
+    cindi = maui.passengers.create!(name: "Cindi", age: "63", gold_status: true)
+    harry = maui.passengers.create!(name: "Harry", age: "35", gold_status: false)
+    larry = maui.passengers.create!(name: "Larry", age: "28", gold_status: true)
+    mary = maui.passengers.create!(name: "Mary", age: "18", gold_status: false)
 
     visit '/flights'
 
