706488e5ffee214049989bbe1311369dd28c29de
Lucas Colwell <colwellc37@gmail.com>
2022-08-22 22:04:14 -0600

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/flights', to: 'flights#index'
+  get '/flights/:id', to: 'flights#show'
 end
