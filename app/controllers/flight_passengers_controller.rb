d789b8f68d76c474049d33df60a867be6aeb4265
Lucas Colwell <colwellc37@gmail.com>
2022-08-30 21:50:06 -0600

    Feat: add CRUD to controller

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -3,4 +3,31 @@ class FlightPassengersController < ApplicationController
     @flight = Flight.find(params[:id])
     @passengers = @flight.passengers
   end
+
+  def new
+    @flight = Flight.find(params[:flight_id])
+  end
+
+  def create
+    passenger = Passenger.create(passenger_params)
+    redirect_to "/flights/#{passenger.flight_id}/passengers"
+    # require "pry"; binding.pry
+  end
+
+  private
+
+  def flight_params
+    params.permit(:number,
+                  :airline,
+                  :destination,
+                  :international)
+  end
+
+  def passenger_params
+    params.permit(:name,
+                  :age,
+                  :gold_status,
+                  :flight_id)
+
+  end
 end
